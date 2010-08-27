f28cb5d21371c454eeddb20324381847ca5430fe
Chris Boulton <chris.boulton@interspire.com>
2010-08-27 17:00:29 +1000

    Whitespace changes

--- src/main/java/com/bigcommerce/fecru/plugins/eventexecutor/ConfigurationManager.java
+++ src/main/java/com/bigcommerce/fecru/plugins/eventexecutor/ConfigurationManager.java
@@ -3,7 +3,7 @@ package com.bigcommerce.fecru.plugins.eventexecutor;
 public interface ConfigurationManager {
 	String getEventCommand(String hook);
 	void storeEventCommand(String hook, String value);
-	
+
 	String loadRunAsUser();
 	void storeRunAsUser(String username);
 }
